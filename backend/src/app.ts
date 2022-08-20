ae710acf2c9bf7696af88b1e4524c9c333214d4d
David Chavez <depch47@gmail.com>
2022-08-20 18:56:57 -0500

    Setting up types

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,14 @@
-console.log("testing two")
+import express, { Express, Response, Request } from 'express'
+
+const app: Express = express()
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({ test: 'hi' })
+})
+
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+})
