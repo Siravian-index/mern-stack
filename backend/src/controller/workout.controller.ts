99b016abc3730e380e69810ad2cb5035dd56fd15
David Chavez <depch47@gmail.com>
2022-08-21 15:49:47 -0500

    ADD mongo uri and mongosse

--- backend/src/controller/workout.controller.ts
+++ backend/src/controller/workout.controller.ts
@@ -1,25 +1,30 @@
 import { Request, Response } from "express"
 
 export const getAll = (req: Request, res: Response) => {
-  return res.json({ controller: true })
+  return res.json({ msg: "getAll" })
 }
 
 export const getById = (req: Request, res: Response) => {
+  return res.json({ msg: "getById" })
 
 }
 
 export const createOne = (req: Request, res: Response) => {
+  return res.json({ msg: "createOne" })
 
 }
 
 export const deleteAll = (req: Request, res: Response) => {
+  return res.json({ msg: "deleteAll" })
 
 }
 
 export const deleteById = (req: Request, res: Response) => {
+  return res.json({ msg: "deleteById" })
 
 }
 
 export const editOne = (req: Request, res: Response) => {
+  return res.json({ msg: "editOne" })
 
 }
