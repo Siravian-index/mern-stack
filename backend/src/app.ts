3e9f2b25ca1d30087f4b5ec48f7b31f862c7d7e1
David Chavez <depch47@gmail.com>
2022-08-21 15:57:42 -0500

    ADD mongoDB connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,19 @@
 import 'dotenv/config'
 import express, { Response, Request } from 'express'
+import connectToDB from './db/config'
 
 import workoutRouter from './routes/workout.routes'
 
-const app = express()
+const main = async () => {
+  const app = express()
 
-app.use(express.json())
+  app.use(express.json())
 
-// custom routes
-app.use('/api/workout', workoutRouter)
+  // custom routes
+  app.use('/api/workout', workoutRouter)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on ${process.env.PORT}`);
-})
+  await connectToDB(app)
+}
+
+main()
+
