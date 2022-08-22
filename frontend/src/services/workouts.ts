db8b0438651d9d22f31a39480aa9e6e515797209
David Chavez <depch47@gmail.com>
2022-08-22 16:02:57 -0500

    Scrapped the whole idea and just YOLO the fetch request

--- frontend/src/services/workouts.ts
+++ frontend/src/services/workouts.ts
@@ -1,22 +1,5 @@
-import { IWorkoutResponse } from "../types/fetchResponse"
 import { IWorkout } from "../types/workout"
 
 const ENDPOINT = 'http://localhost:4000/api/workout/'
 
-
-
-export const getWorkouts = async (): Promise<IWorkoutResponse> => {
-  const response = await fetch(ENDPOINT)
-  if (response.ok) {
-    const data = await response.json() as IWorkout[]
-    const payload = {
-      error: null,
-      data
-    }
-    return payload
-  }
-  return {
-    error: `Server failed to respond. Status: ${response.status}`,
-    data: [],
-  }
-}
+export const getWorkouts = async () => await (await fetch(ENDPOINT)).json() as IWorkout[]
--- frontend/src/types/fetchResponse.ts
+++
@@ -1,6 +0,0 @@
-import { IWorkout } from "./workout"
-
-export interface IWorkoutResponse {
-  error?: string | null
-  data: IWorkout | IWorkout[]
-}
