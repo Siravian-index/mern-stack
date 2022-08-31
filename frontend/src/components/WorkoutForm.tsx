30b78ccfbfb327eb7d43f1fdce83945859de6744
David Chavez <depch47@gmail.com>
2022-08-31 11:00:11 -0500

    Working on improving error handling/msg

--- frontend/src/components/WorkoutForm.tsx
+++ frontend/src/components/WorkoutForm.tsx
@@ -1,34 +1,45 @@
-import { useState } from "react";
 import { useWorkoutContext } from "../context/WorkoutContext";
-import Input from "./Input";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+
+type Inputs = {
+  title: string,
+  reps: number,
+  load: number,
+};
 
 const WorkoutForm = () => {
   const { addItem, errorPost, } = useWorkoutContext()
-  const [title, setTitle] = useState('')
-  const [reps, setReps] = useState('');
-  const [load, setLoad] = useState('');
-
-  const clearInputs = () => {
-    setTitle('')
-    setReps('')
-    setLoad('')
-  }
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const success = await addItem({ title, reps: Number(reps), load: Number(load) })
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const success = await addItem(data)
     if (success) {
-      clearInputs()
+      reset()
     }
   }
 
+  const showButton = !Object.keys(errors).length
   return (
-    <form className="create" onSubmit={handleSubmit}>
+    <form className="create" onSubmit={handleSubmit(onSubmit)}>
       <h3>Add a New Workout</h3>
-
-      <button>Add Workout</button>
-    </form>
+      <label>
+        Title
+        <input {...register("title", { required: { value: true, message: 'This field is required' } })} placeholder='Exercise name' />
+      </label>
+      {errors.title && <p className="error">{errors.title?.message}</p>}
+      <label>
+        Load
+        <input type='number' {...register("load", { required: { value: true, message: 'This field is required' }, valueAsNumber: true, min: { value: 1, message: 'Should be a positive number' }, max: { value: 300, message: 'Should be less than 300' } })} placeholder='Weight' />
+      </label>
+      {errors.load && <p className="error">{errors.load?.message}</p>}
+      <label>
+        Reps
+        <input type='number' {...register("reps", { required: { value: true, message: 'This field is required' }, valueAsNumber: true, min: { value: 1, message: 'Should be a positive number' }, max: { value: 100, message: 'Max reps 100' } })} placeholder='Repetitions' />
+      </label>
+      {errors.reps && <p className="error">{errors.reps?.message}</p>}
+      {showButton && <button> Add Workout</button>}
+      {errorPost && <p className="error">{errorPost}</p>}
+    </form >
   )
 }
 
