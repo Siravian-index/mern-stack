c123b7797db7f013433336770276efcc4320f3a8
David Chavez <depch47@gmail.com>
2023-07-15 12:08:34 -0500

    testing token

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,6 +3,7 @@ import { loginUser, signUpUser } from "../controller/user.controller";
 
 const router = Router()
 
+// teting token
 router.post('/login', loginUser)
 
 router.post('/signup', signUpUser)
